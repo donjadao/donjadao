4f6d6c5646d983a8c202915416490c8bd0f8d1a2
donjadao <ding.don908@gmail.com>
2025-10-13 01:16:40 -0500

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,8 @@
 import { ArrowDown, Github, Linkedin, Mail } from 'lucide-react';
 import { Button } from './ui/button';
 import logo from 'figma:asset/7ce734f2c2e6165613eedbecbb47049bc56bbf5f.png';
-import RippleLogo from '../components/RippleLogo';
-
+// import RippleLogo from '../components/RippleLogo';
+      // <RippleLogo />
 export function Hero() {
   const scrollToProjects = () => {
     const element = document.querySelector('#projects');
@@ -14,7 +14,6 @@ export function Hero() {
       <div className="max-w-4xl mx-auto text-center">
         <div className="mb-8">
           <div className="flex justify-center mb-8">
-           // <RippleLogo />
             <img src={logo} alt="Logo" className="h-48 w-auto" />
           </div>
           <h1 className="mb-4 text-white">
